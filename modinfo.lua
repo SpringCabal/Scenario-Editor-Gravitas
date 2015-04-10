d8a22364438c72098af25ca5f409d2249e1b2396
Gajo Petrovic <gajo01@gmail.com>
2015-04-11 04:41:31 +0900

    initial commit

---
+++ modinfo.lua
@@ -0,0 +1,14 @@
+return {
+  name='Scenario Editor Corruption',
+  description='Scenario Editor for Corruption',
+  shortname='SE_CR',
+  version='git',
+  mutator='Official',
+  game='Scenario Editor Corruption',
+  shortGame='SE_CR',
+  modtype=1,
+  depend = {
+    'Corruption Warmup',
+	'Scenario Editor Core git',
+  },
+}
